1051670d463966e68151f666c7aaa042a8bae2c1
taylorletsoaka <taylorletsoaka@gmail.com>
2018-12-24 20:32:24 +0200

    strings.rb

---
+++ strings.rb
@@ -0,0 +1,9 @@
+string = "I love ruby"
+puts(string.ljust(10))
+puts(string.rjust(10))
+puts(string.center(5))
+puts(string.start_with?("I"))
+puts(string.index("I").zero?)
+list = ["Taylor", "Letsoaka"]
+name = list.join(" ")
+puts(name)
